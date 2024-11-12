fe5d16b1d3d9e6839bfe43c13d8ad40ed2e0bff9
Ashraful Alam <>
2024-11-11 22:20:35 -0500

    Just file name changed

--- app/src/main/java/com/example/otchallenge/di/AppComponent.kt
+++ app/src/main/java/com/example/otchallenge/di/AppComponent.kt
@@ -1,7 +1,5 @@
 package com.example.otchallenge.di
 
-import com.example.excercise.models.network.RemoteApi
-import com.example.otchallenge.utils.NetworkChecker
 import com.example.otchallenge.views.MainActivity
 import dagger.Component
 import javax.inject.Singleton
@@ -9,5 +7,5 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [AppModule::class, NetworkModule::class])
 interface AppComponent {
-    fun inject(mainActivity: MainActivity)
+    fun inject(activity: MainActivity)
 }
